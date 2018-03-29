87309c789a6422d999c9467f992658a0ab8bd936
Michael G. Lerner <mglerner@protonmail.com>
2018-03-29 14:40:22 -0400

    refactored, changed how textbooks are entered, also everything is now lists

--- makesyllabus.py
+++ makesyllabus.py
@@ -4,15 +4,13 @@ import pandas as pd
 import glob
 import urllib
 
-from IPython.display import Image
-from IPython.core.display import HTML 
-
 import requests
 from pptx import Presentation
 from pptx.util import Inches
 
+known_textbooks = ['Knight, Physics for Scientists and Engineers: A Strategic Approach with Modern Physics, 3rd Edition',
+                   'Knight, College Physics: A Strategic Approach, 2nd Edition']
 
-knight = pd.read_csv('Textbooks/Knight3rdEdition.csv')
 
 def scientistparts(fname):
     """The scientist file looks like
@@ -28,142 +26,200 @@ def scientistparts(fname):
     ...
 
     This is an iterator that returns the (item name, content)
-
-    The format keeps Chapter, Section, etc in the Textbook line because otherwise we have duplication (one for each textbook).
+    We expect to combine things (e.g. all of the lines in the 
+    description) later. But not now, because, e.g., each line 
+    in the textbook part is a separate textbook.
     """
     item_name,content = None,[]
-
-    def combinecontent(content):
-        return ' '.join([i.strip() for i in content if i.strip()])
 
     for line in open(fname):
         if line.startswith('#'):
             if item_name is not None:
-                yield (item_name,combinecontent(content))
+                yield (item_name,[i for i in content if i.strip()])
             item_name = line.replace('#','').strip()
             content = []
         else:
-            content.append(line)
+            content.append(line.strip())
     if item_name is not None:
-        yield(item_name,combinecontent(content))
+        yield(item_name,[i for i in content if i.strip()])
 
-def bookparts(txt):
-    parts = txt.split(',')
-    textbook,chapter,section = [],None,None
-    for part in parts:
-        _part = part.lower().strip()
-        if _part.startswith('chapter'):
-            chapter = int(_part.replace('chapter','').strip())
-        elif _part.startswith('section'):
-            section = int(_part.replace('section','').strip())
-        else:
-            textbook.append(part)
-    return({'Textbook':','.join(textbook),
-        'Chapter':chapter,
-        'Section':section})
-    
+class Scientist:
+    def __init__(self,fname):
+        #self._scientistdict = dict(scientistparts(fname))
+        # Can't quite do that because older format had you 
+        # putting in two #Textbook parts instead of just two 
+        # lines under #textbook, for instance.
 
-
-def addscientists(textbook,textbookname,verbose=False):
-    """Handles special formatting for images and links. Also creates PPTX presentation, as a side effect.
-    """
-    prs0 = Presentation()
-    scientists = glob.glob('Scientists/*.txt')
-    for scientist in scientists:
-        if verbose:
-            print('adding scientist',scientist)
-        #parts = dict(scientistparts(scientist))
-        # Above doesn't work if we have duplicates
-        parts ={'Textbook':[]}
-        for (k,v) in scientistparts(scientist):
-            if k == 'Photo':
-                #v = Image(url=v)
-                vv = ''
-                for vvv in v.split():
-                    vv += '<a href="Textbooks/{n}.pptx"><img src="{v}" width="300"/></a>'.format(
-                        v=vvv,
-                        #n=urllib.parse.quote_plus(parts['Name']),
-                        n=parts['Name'],
-                    )
-                v = vv
+        # So, everything in here is assumed to be a list. It's a little 
+        # inconvenient, but I think it's worth it in case people edit the
+        # markdown/txt files by hand. OTOH, just saying "put in multiple lines"
+        # would work and simplify this a bit.
+        self._scientistdict = {}
+        self._fname = fname
+        for (k,v) in scientistparts(fname):
+            if k in self._scientistdict:
+                self._scientistdict[k].append(v)
+            else:
+                self._scientistdict[k] = [v]
+        self.name = self._scientistdict['Name'][0][0].strip()
+    def matchestextbook(self,textbookname,verbose=False):
+        # Should match title. For now, just match edition
+        # If matches, return chapter and section
+        # This is why I need a dropdown for "known" textbooks. And I need 
+        # to fix the others via a script.
+        # Right now, we only know Knight 3rd edition (that's calc)
+        # and Knight 2nd edition (that's alg.)
+        for entry in self._scientistdict['Textbook']:
+            for txt in entry:
+                if txt.startswith(textbookname):
+                    parts = txt.replace(textbookname,'').split(',')
+                    chapter,section = None,None
+                    for part in [p for p in parts if p]:
+                        _part = part.lower().strip()
+                        if _part.startswith('chapter'):
+                            chapter = int(_part.replace('chapter','').strip())
+                        elif _part.startswith('section'):
+                            section = int(_part.replace('section','').strip())
+                        else:
+                            print('Unknown textbook part',part)
+                    return({'Chapter':chapter,
+                            'Section':section})
+        return False
+    def todf(self,chapter,section):
+        # See comments in __init__ but things may be nested one
+        # level deeper than you expect.
+        parts = {'Chapter':chapter,
+                'Section':section}
+        for (k,v) in self._scientistdict.items():
+            if k == 'Textbook':
+                continue
+            elif k == 'Photo':
+                # Can be multiple photos. 
+                # Each gets a link to the original source of the photo, and we follow
+                # it all up with a URL to download the slide.
+                for photos in v:
+                    for photo in photos:
+                        this_entry = f'<a href="{photo}"><img src="{photo}" width="300"/></a>'
+                        if k not in parts:
+                            parts[k] = this_entry
+                        else:
+                            parts[k] = ' '.join([parts[k],this_entry])
             elif k == 'Sources':
-                i = 1
-                formatted = ''
-                for vv in v.split():
-                    if vv.startswith('http'):
-                        linkpart = vv.strip()
-                        if linkpart.endswith('.'):
-                            linkpart = linkpart[:-1]
-                        formatted += '<a href="{vv}">Link{i}</a> '.format(vv=linkpart,i=i)
-                        i += 1
-                    else:
-                        formatted += ' ' + vv
-                v = formatted
-            if k not in parts:
-                parts[k] = v
+                # Wrap it up in a link.
+                for sources in v:
+                    for source in sources:
+                        formatted = ''
+                        for sourcepart in source.split():
+                            if sourcepart.startswith('http'):
+                                linkpart = sourcepart.strip()
+                                if linkpart.endswith('.'):
+                                    linkpart = linkpart[:-1]
+                                formatted +=  f' <a href="{linkpart}">Link</a> '
+                            else:
+                                formatted += ' ' + sourcepart
+                        if 'Sources' in parts:
+                            parts['Sources'] += formatted
+                        else:
+                            parts['Sources'] = formatted
             else:
-                parts[k].append(v)
-        for tb in parts['Textbook']:
-            bp = bookparts(tb)
-            if bp['Textbook'] == textbookname:
-                parts.update(bp)
-                df = pd.DataFrame(data=[parts])
-                del df['Textbook']
-                if verbose:
-                    print('df',df)
-                textbook = textbook.append(df)
-            else:
-                if verbose: print('skipping',bp['Textbook'],'because it is not',textbookname)
+                # v looks like [['Myname']] or [['Description','Description','more description']]
+                for entry in v:
+                    this_entry = ' '.join(entry)
+                if k not in parts:
+                    parts[k] = this_entry
+                else:
+                    parts[k] = ' '.join([parts[k],this_entry])
+        if 'Photo' in parts:
+            slide_link = '<a href="Textbooks/{n}.pptx">Download Slide</a>'.format(
+                        n=parts['Name'],)
+            parts['Photo'] = parts['Photo'] + ' ' + slide_link
+        #for (k,v) in parts.items():
+        #    print(f'{k}: {v}')
+        df = pd.DataFrame(data=[parts])
+        return df
 
-        # Now, for extra cool, make a powerpoint slide!
-        # This should loop through each of the photos, making a new slide in the presentation for each photo.
-        if 'Photo' in parts and parts['Photo']:
-            prs = Presentation()
-            add_scientist_slide(prs0,scientist,name=parts['Name'],verbose=verbose)
-            add_scientist_slide(prs,scientist,name=parts['Name'],verbose=verbose)
-            # get the original, unparsed photos bit.
-            prs.save('Textbooks/{n}.pptx'.format(n=parts['Name'].strip()))
-    prs0.save('Textbooks/{n}.pptx'.format(n=textbookname))
-    return textbook
 
-def add_scientist_slide(prs,scientist,name,verbose=False):
+def add_scientist_slide(prs,scientist,verbose=False):
     """Creates image file as side effect
     """
-    for (k,v) in scientistparts(scientist):
-        if k == 'Photo':
-            break
-    for p in v.split():
-        img_path = 'test.jpg'
-        if verbose:
-            print(f'Grabbing image: {p}')
-        img_data = requests.get(p).content
-        with open(img_path, 'wb') as handler:
-            handler.write(img_data)
-        blank_slide_layout = prs.slide_layouts[5]
+    for photos in scientist._scientistdict['Photo']:
+        for photo in photos:
+            img_path = 'test.jpg'
+            if verbose:
+                print(f'Grabbing image: {photo}')
+            img_data = requests.get(photo).content
+            with open(img_path, 'wb') as handler:
+                handler.write(img_data)
+            blank_slide_layout = prs.slide_layouts[5]
+            slide = prs.slides.add_slide(blank_slide_layout)
 
-        slide = prs.slides.add_slide(blank_slide_layout)
+            shapes = slide.shapes
+            title_shape = shapes.title
+            title_shape.text = scientist.name
 
-        shapes = slide.shapes
-        title_shape = shapes.title
-        title_shape.text = name
+            top = Inches(1.75)
+            left = Inches(0.5)
+            height = Inches(5)
+            pic = slide.shapes.add_picture(img_path, left, top, height=height)
 
-        top = Inches(1.75)
-        left = Inches(0.5)
-        height = Inches(5)
-        pic = slide.shapes.add_picture(img_path, left, top, height=height)
+            notes_slide = slide.notes_slide
+            text_frame = notes_slide.notes_text_frame
+            text_frame.text = open(scientist._fname).read()
 
-        notes_slide = slide.notes_slide
-        text_frame = notes_slide.notes_text_frame
-        text_frame.text = open(scientist).read()
-    
+def maketextbook(fname,*,textbookname,scientists,verbose=False):
+    """This also makes the associated PPTx slides.
+    """
+    textbook = pd.read_csv(fname)
+    prs0 = Presentation() # The whole textbook. Individual slides made separately below.
+    for scientist in scientists:
+        match = scientist.matchestextbook(textbookname)
+        if match:
+            chapter,section = match['Chapter'],match['Section']
+            if verbose:
+                print('Adding',scientist._scientistdict['Name'])
+            df = scientist.todf(chapter=chapter,section=section)
+            textbook = textbook.append(df)
+            
+            if 'Photo' in scientist._scientistdict:
+                prs = Presentation()
+                add_scientist_slide(prs0,scientist,verbose=verbose)
+                add_scientist_slide(prs,scientist,verbose=verbose)
+                # get the original, unparsed photos bit.
+                prs.save('Textbooks/{n}.pptx'.format(n=scientist.name))
 
-def maketextbook(fname,verbose=False):
-    d = pd.read_csv(fname)
-    textbook = addscientists(d,'Knight, 3rd edition',verbose=verbose)
+
     # I want the order of columns
     cols = ['Chapter','Section','Topics','Photo','Name','Description','Sources']
     cols = cols + [i for i in textbook.columns.tolist() if i not in cols]
     textbook = textbook[cols]
     textbook = textbook.sort_values(by=['Chapter','Section','Topics','Description','Name'],
                                     ascending=[True, True, True, False, False])
-    return textbook.fillna('').set_index(['Chapter', 'Section','Topics'])
+    textbook = textbook.fillna('').set_index(['Chapter', 'Section','Topics'])
+
+    # Github doesn't quite do what I expect with the table
+    # rendering. Specifically, I can't seem to just specify the size of
+    # a photo either in the `img` tag or in the `th` element. I've
+    # tried both CSS and HTML, but what seems to actually work is
+    # setting it in HTML or CSS ... and then making the column header
+    # wider.
+    spaces = '&nbsp;'*50
+    textbook.rename(columns={'Photo':'Photo'+spaces,'Description':'Description'+spaces+spaces}, inplace=True)
+    prs0.save(f'Textbooks/{textbookname}.pptx')    
+    return textbook
+
+
+def makeKnightCalc3rd():
+    """Creates all of the PPT files as a side-effect, returns the HTML
+    """
+    scientists = [Scientist(fname) for fname in glob.glob('Scientists/*.txt')]
+    textbook = maketextbook('Textbooks/Knight3rdEdition.csv',
+                            textbookname=known_textbooks[0],
+                            scientists=scientists)
+    with pd.option_context('display.max_colwidth', -1):
+        output_html = textbook.to_html(escape=False)
+
+    f = open('knight.html','w')
+    f.write(output_html)
+    f.close()
+    return(output_html)
+
